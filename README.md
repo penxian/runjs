a53fdc3fefce10474b483cd252a62d5f75018fac
richman <blueskyred2006@163.com>
2015-08-28 16:56:46 +0800

    Signed-off-by: richman <blueskyred2006@163.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # runjs
 Ediort JS Online
+
+[http://www.365mini.com/diy.php](http://www.365mini.com/diy.php)
--- index.js
+++ index.js
@@ -20,8 +20,18 @@ require([
  	var htmlcodeMirror = CodeMirror.fromTextArea(document.getElementById('htmlcode'),{
 		mode:'htmlmixed',
 		lineNumbers:true,
+		indentUnit: 4, // 缩进
+		smartIndent: true, // 智能缩进
+		styleActiveLine: true, // 当前行高亮
+		autofocus: false, // 自动获得焦点
 		lineWrapping:false,
+		autoCloseBrackets: true,// 自动关闭括号
+		autoCloseTags: true, // 自动关闭标签
+		matchTags:{ bothTags: true },
+		matchBrackets: true,
 		theme:"eclipse"
+		,foldGutter: true
+		,gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers", "CodeMirror-foldgutter"]
 	});
 
 	var javascript = CodeMirror.fromTextArea(document.getElementById('jscode'),{
